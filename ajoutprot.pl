475b2430dbb39e1e08f9e880c44a40fb0a587b8e
Elsa Claude <elclaude@emi.u-bordeaux1.fr>
2019-03-29 14:47:58 +0100

    creation fichier pour ajout et modif d'une proteine

---
+++ ajoutprot.pl
@@ -0,0 +1,25 @@
+#!bin/perl
+use warnings;
+use strict;
+use DBI;
+use Switch;
+
+my $dbh = DBI->connect("DBI:Pg:dbname=elclaude;host=dbserver","elclaude","*Cochon04111997",{'RaiseError' => 1});
+
+sub insert_protein() {
+  my @protein;
+  my @ecran=("Veuillez entrez : \n Entry","Entry Name","Status (reviewed ou unreviewed)","Organisme","EnsemblePlantTranscript","ProteinNames","Length","Sequence"); ## <=  toutes les caractéristiques à demander à l'utilisateur pour remplir la table générale et la table protéine
+  for my $e (@ecran) {
+      print "$e : ";
+      my $usr=<STDIN>;
+      chomp($usr);
+      push(@protein,$usr);
+    }
+  my $insert_general = $dbh->do("INSERT INTO Caractéristiques_générales_UniProt VALUES ('$protein[0]','$protein[1]','$protein[2]','$protein[3]','$protein[4]')");
+  my $insert_protein = $dbh->do("insert into Informations_Protéines_UniProt values('$protein[0]','$protein[5]','$protein[6]','$protein[7]')"); # remplacer avec les bons éléments de @protéine
+  ## faire 2e requête pour remplir la table générale avec les bons éléments de @protein
+}
+
+insert_protein();
+
+$dbh->disconnect();
---
+++ modifsequence.pl
@@ -0,0 +1,21 @@
+#!bin/perl
+use warnings;
+use strict;
+use DBI;
+use Switch;
+
+my $dbh = DBI->connect("DBI:Pg:dbname=elclaude;host=dbserver","elclaude","*Cochon04111997",{'RaiseError' => 1});
+
+sub modif_sequence(){
+  print("Veuillez entrez le nom \"Entry\" de la protéine dont vous souhaitez modifier la séquence :\n");
+  my $prot = <STDIN>;
+  chomp($prot);
+  print("Entrez maintenant la séquence complète corrigée :");
+  my $seq = <STDIN>;
+  chomp($seq);
+  my $modif_sequence = $dbh->do("UPDATE Informations_Protéines_UniProt SET Sequence = '$seq' WHERE Entry = '$prot'");
+}
+
+modif_sequence();
+
+$dbh->disconnect();
